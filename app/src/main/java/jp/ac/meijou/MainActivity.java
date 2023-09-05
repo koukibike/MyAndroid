802df5c75d191c747b250052129916a3d700d87c
koukibike <221205189@ccmailg.meijo-u.ac.jp>
2023-09-05 15:42:06 +0900

    データストアの仕組みを実装

--- app/src/main/java/jp/ac/meijou/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/MainActivity.java
@@ -5,23 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.TextView;
 
 import jp.ac.meijou.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.button.setOnClickListener(View -> {
+        prefDataStore = PrefDataStore.getInstance(this);
+        //prefDataStore.getString("name").ifPresent(name -> binding.text.setText(name));
+        binding.saveButton.setOnClickListener(View -> {
             var text = binding.editText.getText().toString();
-            binding.text.setText(R.string.my_name);
+            prefDataStore.setString("name",text);
+            Log.d("kouki","save:" + binding.editText);
         });
-
+        Log.d("kouki","onCreate text:"+binding.text.getText());
         binding.editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
@@ -35,10 +40,15 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-                binding.text.setText(editable.toString());
+                //binding.text.setText(editable.toString());
 
             }
         });
-
+        //log.d("kouki","onCreate text:"+binding.text.getText());
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        prefDataStore.getString("name").ifPresent(name -> binding.text.setText(name));
     }
 }
