d42658cd9603a02333b867c4465d8b54e17c3001
koukibike <221205189@ccmailg.meijo-u.ac.jp>
2023-09-05 14:29:11 +0900

    表示される文字列のサイズを大きく変更

--- app/src/main/java/jp/ac/meijou/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/MainActivity.java
@@ -3,12 +3,42 @@ package jp.ac.meijou;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+
+import jp.ac.meijou.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.button.setOnClickListener(View -> {
+            var text = binding.editText.getText().toString();
+            binding.text.setText(R.string.my_name);
+        });
+
+        binding.editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.text.setText(editable.toString());
+
+            }
+        });
+
     }
 }
